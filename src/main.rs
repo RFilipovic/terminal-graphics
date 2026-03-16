ba26d5fc8d9a614538ddacbf707a9020a4005f7b
rene <rene.filipovic03@gmail.com>
2026-03-16 17:16:08 +0100

    *Added draw line functionality to make lines on the frame buffer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io::{self, Write};
+use std::thread;
+use std::time::Duration;
 
 const FRAME_WIDTH: usize = 60;
 const FRAME_HEIGHT: usize = 30;
@@ -6,6 +8,11 @@ const FRAME_HEIGHT: usize = 30;
 struct FrameBuffer {
     width: usize,
     height: usize,
+}
+
+struct Point {
+    x: usize,
+    y: usize,
 }
 
 impl FrameBuffer {
@@ -38,7 +45,36 @@ fn set_pixel(render_data: &mut [char], x: usize, y: usize, ch: char) {
         println!("x must be less that FRAME_WIDTH, y must be less than FRAME_HEIGHT")
     } else {
         render_data[y * FRAME_WIDTH + x] = ch;
-        println!("{}", serialize(render_data, FRAME_WIDTH, FRAME_HEIGHT));
+    }
+}
+
+fn draw_line(point0: Point, point1: Point, render_data: &mut [char]) {
+    let mut x0 = point0.x as isize;
+    let mut y0 = point0.y as isize;
+    let x1 = point1.x as isize;
+    let y1 = point1.y as isize;
+
+    let dx = (x1 - x0).abs();
+    let dy = (y1 - y0).abs();
+    let sx = if x0 < x1 { 1 } else { -1 };
+    let sy = if y0 < y1 { 1 } else { -1 };
+
+    let mut err = dx - dy;
+    loop {
+        set_pixel(render_data, x0 as usize, y0 as usize, '#');
+        if x0 == x1 && y0 == y1 {
+            break;
+        }
+
+        let e2 = 2 * err;
+        if e2 > -dy {
+            err -= dy;
+            x0 += sx;
+        }
+        if e2 < dx {
+            err += dx;
+            y0 += sy;
+        }
     }
 }
 
@@ -46,13 +82,28 @@ fn main() -> Result<(), std::io::Error> {
     let mut out = io::stdout();
     let fb = FrameBuffer::new(FRAME_WIDTH, FRAME_HEIGHT);
 
-    let mut render_data: Vec<char> = vec!['.'; fb.width * fb.height];
+    let mut render_data: Vec<char> = vec![' '; fb.width * fb.height];
+    /*
+        loop {
+            write!(out, "\x1b[2J")?;
+            out.flush()?;
 
-    set_pixel(&mut render_data, 59, 29, 'O');
-    set_pixel(&mut render_data, 0, 0, 'O');
+            write!(out, "\x1b[H")?;
+            out.flush()?;
 
-    write!(out, "\x1b[2J")?;
-    out.flush()?;
+            println!("{}", serialize(&render_data, FRAME_WIDTH, FRAME_HEIGHT));
+        }
+    */
+
+    let point0 = Point { x: 29, y: 29 };
+    let point1 = Point { x: 0, y: 0 };
+    draw_line(point0, point1, &mut render_data);
+
+    let point0 = Point { x: 59, y: 0 };
+    let point1 = Point { x: 0, y: 29 };
+    draw_line(point0, point1, &mut render_data);
+
+    println!("{}", serialize(&render_data, FRAME_WIDTH, FRAME_HEIGHT));
 
     Ok(())
 }
